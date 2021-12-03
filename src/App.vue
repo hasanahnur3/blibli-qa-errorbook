1105837631b2ba72d4ffe8b89ef2da85c808b5d9
Nur Fauziah Hasanah <hasanahnur@msani.net>
2021-12-03 08:29:59 +0700

    feat: add meta with toml

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
 <template>
+
+  <html>
+  <head>
+    <meta http-equiv="Content-Security-Policy" content="default-src 'self';
+      script-src 'unsafe-inline';">
+  </head>
+  <body>
   <div id="app">
     <div class="row">
       <div class="col-1"></div>
@@ -7,12 +14,24 @@
       </div>
       <div class="col-1"></div>
     </div>
+  </div>
+  </body>
+  </html>
+
+<!--  <div id="app">-->
+<!--    <div class="row">-->
+<!--      <div class="col-1"></div>-->
+<!--      <div class="col-10">-->
+<!--        <router-view />-->
+<!--      </div>-->
+<!--      <div class="col-1"></div>-->
+<!--    </div>-->
 
     <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div> -->
-  </div>
+<!--  </div>-->
 </template>
 
 <style lang="scss">
